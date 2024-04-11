697d4c414d0e08815e048ec1c0c260ac539ffd50
Somondas <somondas067@gmail.com>
2024-04-11 16:54:21 +0530

    working on store modal

--- components/modals/store-modals.tsx
+++ components/modals/store-modals.tsx
@@ -1,12 +1,30 @@
 "use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useStoreModal } from "@/hooks/use-store-modal";
 import React from "react";
 import { Modal } from "@/components/ui/modal";
 
+const formSchema = z.object({
+  name: z.string().min(3),
+  // storeAddress: z.string().min(3),
+});
+
 export const StoreModal = () => {
   const storeModal = useStoreModal();
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      // storeAddress: "",
+    },
+  });
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
   return (
     <Modal
       title="Create Store"
