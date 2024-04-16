91315b86b78cb2e8b0cbfcf635039872ebe30cb7
Somondas <somondas067@gmail.com>
2024-04-16 18:45:40 +0530

    Maybe I should stop here..

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -3,9 +3,11 @@ import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { useStoreModal } from "@/hooks/use-store-modal";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Store } from "@prisma/client";
 import { Trash } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import * as z from "zod";
 
 interface SettingsFormProps {
@@ -15,7 +17,15 @@ const formSchema = z.object({
   name: z.string().min(1),
 });
 type SettingsFormValues = z.infer<typeof formSchema>;
+
 const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
   const storeModal = useStoreModal();
   return (
     <>
